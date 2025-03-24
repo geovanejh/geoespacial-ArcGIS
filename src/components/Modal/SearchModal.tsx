d869bb86c5a8045e888f5ac0a6b9d5da4b5d2ccb
geovanejh <geovanehartmann@gmail.com>
2025-03-24 18:05:35 -0300

    refactor Header and SearchModal components; add bairro search functionality in Home page

--- src/components/Modal/SearchModal.tsx
+++ src/components/Modal/SearchModal.tsx
@@ -10,18 +10,38 @@ import {
 interface SearchModalProps {
   isOpen: boolean;
   onClose: () => void;
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  buscaBairro: () => Promise<void>;
 }
 
-export const SearchModal = ({ isOpen, onClose }: SearchModalProps) => {
+export const SearchModal = ({
+  isOpen,
+  onClose,
+  search,
+  setSearch,
+  buscaBairro,
+}: SearchModalProps) => {
   if (!isOpen) return null;
+
+  const handleSearch = () => {
+    console.log("executou");
+    buscaBairro();
+    onClose();
+  };
 
   return (
     <ModalOverlay onClick={onClose}>
       <ModalContent onClick={(e) => e.stopPropagation()}>
-        <ModalTitle>Buscar bairro</ModalTitle>
+        <ModalTitle>Buscar Bairro</ModalTitle>
         <CloseButton onClick={onClose}>×</CloseButton>
-        <SearchInput placeholder="Digite o nome do bairro" />
-        <SearchButton>Buscar</SearchButton>
+        <SearchInput
+          placeholder="Nome do Bairro"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <SearchButton onClick={handleSearch}>Buscar</SearchButton>
       </ModalContent>
     </ModalOverlay>
   );
